8a2306e55ae5ce880368c1f94cfcc5be6fdebb1e
angeiradu <a.iradukund2@alustudent.com>
2023-02-10 07:36:56 -0800

    8-uppercase.py

--- python-if_else_loops_functions/8-main.py
+++ python-if_else_loops_functions/8-main.py
@@ -1,1 +1,4 @@
-
+#!/usr/bin/python3
+wisdom = __import__( '8-uppercase' ).uppercase
+wisdom("best")
+wisdom("Best School 98 Battery street")
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,3 @@ def uppercase(str):
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
             c = chr(ord(c) - 32)
         print("{}".format(c), end="")
-     print("")
-
