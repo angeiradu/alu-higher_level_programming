128cce4f6af9f665ee153a8ffac949c127ddf16b
angeiradu <a.iradukund2@alustudent.com>
2023-02-20 10:52:45 -0800

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-from add_0 import add
-=======
+# 0_add.py
+# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
     from add_0 import add
->>>>>>> 1c120f538d60645828b776b8d16daf1ad9310693
 
-a = 1
-b = 2
-
-if __name__ == '__main__':
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
