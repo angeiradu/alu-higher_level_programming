9caf768b4181a436c67188080c4f5fca65940826
angeiradu <a.iradukund2@alustudent.com>
2023-03-02 23:18:21 -0800

    advanced task

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+from __future__ import print_function
 import sys
 
 
 def safe_function(fct, *args):
     try:
-        res = fct(*args)
-        return res
-    except Exception as err:
-        sys.stdout.write("Exception: " + str(err) + "\n")
-        return
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
