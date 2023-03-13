3b8fd10aa5aab322b5136e3b02ec69267a3c1131
angeiradu <a.iradukund2@alustudent.com>
2023-03-13 02:21:13 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# 1-square.py
+"""Private instance attribute."""
+
+
+class Square(object):
+    """ a class variable size."""
+    def __int__(self, size)
+    """initialize size."""
+    self.__size = size
