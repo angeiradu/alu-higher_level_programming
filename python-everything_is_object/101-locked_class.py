c54d94cb1652abd1d68f2be59e6149c040a9bcdb
angeiradu <a.iradukund2@alustudent.com>
2023-03-21 01:01:16 -0700

    advanced task 2

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+class LockedClass:
+   """ prevent the user to create new instance attributes except if the new instance attributes is first_name"""
+
+   __slots__ = ["first_name"]
