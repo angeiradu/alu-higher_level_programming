d18a21391f2b4e2a06286212ab47f192352402d1
angeiradu <a.iradukund2@alustudent.com>
2023-03-22 00:58:52 -0700

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""1-my_list.py""'
+
+
+class MyList(list):
+    """represent MyList"""
+
+    def print_sorted(self):
+    """prints the list but sorted"""
+    print(sorted(self))
