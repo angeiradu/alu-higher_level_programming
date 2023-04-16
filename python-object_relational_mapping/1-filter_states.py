fed8551a51cbcf4603619082a56dfdfacfa62cd8
angeiradu <a.iradukund2@alustudent.com>
2023-04-16 12:39:39 -0700

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# import module
+import MySQLdb
+from sys import argv
+
+# arguments
+username = argv[1]
+password = argv[2]
+database = argv[3]
+
+# connect to database
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=database
+    )
+
+# create cursor
+cursor = db.cursor()
+
+# execute query
+cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+
+# fetch all rows
+rows = cursor.fetchall()
+
+# loop through rows
+for row in rows:
+    print(row)
+
+# close connection
+cursor.close()
+db.close()
