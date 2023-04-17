b826b18245cdf42813013a2f1e10228819ebb8c4
angeiradu <a.iradukund2@alustudent.com>
2023-04-16 23:08:51 -0700

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-# import the MySQLdb module
-import MySQLdb
-# takes 3 arguments
-from sys import argv
+"""
+script that lists all states from the database
+hbtn_0e_0_usa
+"""
 
-username = argv[1]
-password = argv[2]
-database = argv[3]
+if __name__ == '__main__':
 
-# connect to the MySQL server
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database
-)
+    import sys
+    import MySQLdb
 
-# create a cursor object
-cur = db.cursor()
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
 
-# execute a query
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-# fetch all rows
-rows = cur.fetchall()
-
-# print rows
-for row in rows:
-    print(row)
-
-# close the cursor
-cur.close()
-
-# close the connection
-db.close()
+    cur.close()
+    db.close()
