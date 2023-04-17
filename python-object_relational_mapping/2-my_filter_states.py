80a7e53998a80f9d47ada3369777ed363c014ff6
angeiradu <a.iradukund2@alustudent.com>
2023-04-17 00:28:16 -0700

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all
+values in the states table of 
+hbtn_0e_0_usa where name matches the argument.
+"""
+
+if __name__ == '__main__':
+
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306,
+        state_name=sys.argv[4]
+    )
+
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name='{}'ORDER BY id ASC".format(state_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
