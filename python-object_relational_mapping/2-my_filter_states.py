e2913c489d5611d2a76179245c99af2572a8b655
angeiradu <a.iradukund2@alustudent.com>
2023-04-17 02:33:44 -0700

    task 1

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Displays all values in the states table of hbtn_0e_0_usa where
-name matches the argument"""
+"""script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
 
-    conn = MySQLdb.connect(host="localhost",
+    db= MySQLdb.connect(host="localhost",
                            port=3306,
                            user=sys.argv[1],
                            passwd=sys.argv[2],
-                           db=sys.argv[3],
-                           charset="utf8")
+                           db=sys.argv[3])
+                    
     # Start cursor
-    cur = conn.cursor()
+    cursor = db.cursor()
 
     # Query
-    cur.execute("""SELECT * FROM states \
+    cursor.execute("""SELECT * FROM states \
         WHERE name= '{}' ORDER BY id ASC""".format(sys.argv[4]))
-    query_rows = cur.fetchall()
+    query_rows = cursor.fetchall()
 
     # Print query
     for row in query_rows:
