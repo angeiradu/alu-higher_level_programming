b2413c4a62c24ea62c8f81d9486f4a0534e5a409
angeiradu <a.iradukund2@alustudent.com>
2023-04-17 01:41:09 -0700

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,9 +15,7 @@ if __name__ == '__main__':
         passwd=sys.argv[2],
         db=sys.argv[3],
         host='localhost',
-        port=3306
-    )
-
+        port=3306)
     cur = db.cursor()
     cur.execute(
             "SELECT * FROM states WHERE name LIKE"
@@ -26,6 +24,5 @@ if __name__ == '__main__':
     for row in rows:       
         if row == sys.argv[4]:
             print(row)
-
     cur.close()
     db.close()
