628f1ca483f9248bc9d8d6cda1966af9496ab8f3
angeiradu <a.iradukund2@alustudent.com>
2023-04-20 00:07:13 -0700

    task 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,11 +8,21 @@ def text_indentation(text):
         text (string): The text to print.
     Raises:
         TypeError: If text is not a string.
-        """
-        if type(text) != str:
-        raise TypeError('text must be a string')
-    for char in text:
-        if char == '.' or char == '?' or char == ':':
-            print(char, '\n\n')
-        else:
-            print(char, end='')
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
