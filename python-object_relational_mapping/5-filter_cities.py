386573aef94471bd974a17e85086276ac876af47
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-04-25 11:39:04 -0700

    Update 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa"""
-import MySQLdb
+"""
+List all cities of a state
+"""
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3])              
-    # Create a cursor
-    cursor = db.cursor()
-    # Execute SQL Query
-    cursor.execute("SELECT cities.name FROM cities\
-        JOIN states ON cities.state_id = states.id\
-            WHERE states.name=%s", (sys.argv[4], ))
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-    query_rows = cursor.fetchall()
-    out = []
-    for row in query_rows:
-        out.append(row[0])
-    # Print query
-    print(', '.join(out))
-
-    # Close cursor and connection
-    cursor.close()
-    db.close()
+    print(", ".join([state[1] for state in states]))
