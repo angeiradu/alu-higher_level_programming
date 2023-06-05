b20d1736dc032c631d97ac9f1df88d13423bd9c0
angeiradu <a.iradukund2@alustudent.com>
2023-06-05 06:09:51 -0700

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const arg= process.argv.length - 2;
+const arg = process.argv.length - 2;
 if (arg === 0) {
     console.log('No argument');
-}
-else if (arg === 1) {
+} else if (arg === 1) {
     console.log('Argument found');
-}
-else {
+} else {
     console.log('Arguments found');
 }
