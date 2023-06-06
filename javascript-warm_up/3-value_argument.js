4e0a6e755e907bca5686a18c5b3626d6dc16d2f9
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-06-06 13:09:18 -0700

    task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,6 +2,11 @@
 
 const arg = process.argv.slice(2);
 
-if (arg === 0){
+if (arg === 0) {
   console.log('No argument');
+} else if (arg === 1) {
+  console.log('HBTN');
+} else {
+  console.log('HBTN cool');
 }  
+  
