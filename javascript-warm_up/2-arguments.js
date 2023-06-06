7dfa783d2623ff897e168687c9729e724d3742c5
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-06-06 10:28:16 -0700

    task -2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const arg = process.argv.length - 2;
 if (arg === 0) {
 	console.log('No argument');
-} else if(arg ===1) {
+} else if (arg === 1) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
