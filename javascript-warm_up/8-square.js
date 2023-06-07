133a9dc75bb317f06073858d028831cea48fe731
angeiradu <a.iradukund2@alustudent.com>
2023-06-07 04:49:09 -0700

    task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+if (isNaN(arg)) {
+  console.log('Missing size');
+} else {
+    const num = parseInt(arg);
+    for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      console.log(X);
+    }}
+}	
