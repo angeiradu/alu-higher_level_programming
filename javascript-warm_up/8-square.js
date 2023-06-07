c90a1d02d76072ebcc5ef673be3f52fd064a627f
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-06-07 05:08:33 -0700

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const arg = process.argv[2];
 if (isNaN(arg)) {
   console.log('Missing size');
 } else {
-    const num = parseInt(arg);
-    for (let i = 0; i < num; i++) {
-    for (let j = 0; j > num; j--) {
+   const num = parseInt(arg);
+   for (let i = 0; i < num; i++) {
+   for (let j = 0; j > num; j--) {
       console.log(' X ');
     }}
 }	
