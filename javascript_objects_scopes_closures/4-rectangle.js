d4237ec6425ced6534d3438428eac79a5b6544b4
angeiradu <a.iradukund2@alustudent.com>
2023-06-09 07:44:04 -0700

    task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+  }
+  
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while(y < this.width) {
+        myVar += 'X';
+	y++;
+      }
+      console.log(myVar);
+    }
+  }
+
+  rotate() {
+    let temp = 0;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double() {
+    width *= 2;
+    height *= 2;
+  }
+  module.exports = Rectangle;	  
