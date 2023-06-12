061d9d937712d6ba4ff5cf46c15a60ba4e26e017
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-06-12 03:32:39 -0700

    task 4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -4,7 +4,8 @@ class Rectangle {
     if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
       this.width = w;
       this.height = h;
-  }
+    }
+  }	  
   
   print() {
     for (let i = 0; i < this.height; i++) {
