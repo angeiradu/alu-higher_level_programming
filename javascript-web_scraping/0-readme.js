a16411d3ac4e094443cb7a9ff478de22304788ca
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-06-13 08:21:08 -0700

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filename = process.argv[3];
-fs.readFile(filename, 'utf-8', (error, content));
+fs.readFile(filename, 'utf-8', (error, content) => {
   if (error) {
     console.log(error);
   } else {
     console.log(content);
-  }
+  });
